01eaf8b7aa4afadd8ce44351cd2c7c8fee7fbc07
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-01-18 11:36:39 +0530

    Updated Hoisting

---
+++ Javascript/Answers/Hoisting.js
@@ -0,0 +1,37 @@
+/*
+	# Hoisting - All the variable declarations happens at the the top. Same goes for function declaration as well.
+	
+	## Reference 
+	1. https://code.tutsplus.com/tutorials/javascript-hoisting-explained--net-15092
+	2. https://www.sitepoint.com/back-to-basics-javascript-hoisting/
+	3. http://javascript.info/tutorial/initialization
+*/
+
+// Example 1
+
+console.log(foo); // Undefined
+var foo = 10;
+console.log(foo); // 10
+
+// Above example goes like below
+
+var foo; // Declaration
+console.log(foo); // Undefined
+foo = 10; // Initialization (happens at the `execution`)
+console.log(foo); // 10
+
+// Example 2
+
+// Calling before declaration.
+bar(); // Bar
+function bar() {
+	console.log("Bar");
+}
+
+// Above example goes like below
+
+// Function declaration happens at the top
+function bar() {
+	console.log("Bar");
+}
+bar(); // Bar
